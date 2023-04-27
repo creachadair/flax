8e76e30dc0ef41c55373275ab0bea3b5f45bbc2c
M. J. Fromberger <michael.j.fromberger@gmail.com>
2023-04-26 21:04:22 -0700

    Add package example.

---
+++ example_test.go
@@ -0,0 +1,29 @@
+package flax_test
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/creachadair/flax"
+)
+
+var flags struct {
+	Debug   bool    `flag:"debug,default=true,Enable debugging output"`
+	Text    string  `flag:"text,default=OK,Text to display"`
+	Rate    float64 `flag:"rate,default=0.1,Rate of increase"`
+	Ignored int
+}
+
+func init() { flax.MustBind(flag.CommandLine, &flags) }
+
+func Example() {
+	flag.Parse()
+
+	if flags.Debug {
+		fmt.Println(flags.Text, flags.Rate)
+	}
+	fmt.Println(flags.Ignored)
+	// Output:
+	// OK 0.1
+	// 0
+}
