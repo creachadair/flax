61cda85f3ac9bb61d22eac70930234f1071d252a
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-11-27 17:07:33 -0800

    flax: add an example for Check

--- example_test.go
+++ example_test.go
@@ -5,6 +5,7 @@ package flax_test
 import (
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/creachadair/flax"
 )
@@ -28,4 +29,41 @@ func Example() {
 	// Output:
 	// OK 0.1
 	// 0
+}
+
+func ExampleCheck() {
+	fields, err := flax.Check(&struct {
+		A int     `flag:"apple,default=*,Apples"`       // use field value as default
+		P int     `flag:"pear,Pears" flag-default:"12"` // set explicit default, tag
+		U string  `flag:"plum,default=$X,Plums"`        // read default from environment
+		C float64 `flag:"cherry,default=0.1,Cherries"`  // set explicit default, inline
+	}{A: 3})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fs := flag.NewFlagSet("test", flag.PanicOnError)
+	fields.Bind(fs)
+
+	fmt.Println("-- fields --")
+	for _, f := range fields { // Fields in declaration order
+		fmt.Println(f.Name, f.Usage)
+	}
+
+	fmt.Println("\n-- flags --")
+	fs.VisitAll(func(f *flag.Flag) { // Flags in name order
+		fmt.Printf("%s %q %v\n", f.Name, f.DefValue, f.Usage)
+	})
+	// Output:
+	// -- fields --
+	// apple Apples
+	// pear Pears
+	// plum Plums
+	// cherry Cherries
+	//
+	// -- flags --
+	// apple "3" Apples
+	// cherry "0.1" Cherries
+	// pear "12" Pears
+	// plum "" Plums [env: X]
 }
